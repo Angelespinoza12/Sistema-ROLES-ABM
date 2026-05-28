9774be525c6c7f496ec3f6359eccabab216c8837
Angelespinoza12 <angelfernandoespinoza2005@gmail.com>
2026-05-28 03:22:14 -0400

    fix username seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,14 +10,55 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+    
         User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
             [
-                'email' => 'omar@gmail.com'
-            ],
+                'nombre' => 'Administrador Principal',
+                'usuario' => 'admin',
+                'password' => Hash::make('Admin123*'),
+                'rol' => 'admin'
+            ]
+        );
+
+
+        User::updateOrCreate(
+            ['email' => 'user1@gmail.com'],
             [
-                'name' => 'omarqm',
-                'username' => 'omarqm',
-                'password' => Hash::make('Omar411*')
+                'nombre' => 'Usuario Uno',
+                'usuario' => 'user1',
+                'password' => Hash::make('123456'),
+                'rol' => 'user'
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'user2@gmail.com'],
+            [
+                'nombre' => 'Usuario Dos',
+                'usuario' => 'user2',
+                'password' => Hash::make('123456'),
+                'rol' => 'user'
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'user3@gmail.com'],
+            [
+                'nombre' => 'Usuario Tres',
+                'usuario' => 'user3',
+                'password' => Hash::make('123456'),
+                'rol' => 'user'
+            ]
+        );
+
+        User::updateOrCreate(
+            ['email' => 'omar@gmail.com'],
+            [
+                'nombre' => 'omarqm',
+                'usuario' => 'omarqm',
+                'password' => Hash::make('Omar411*'),
+                'rol' => 'user'
             ]
         );
     }
